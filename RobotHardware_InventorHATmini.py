efbc9439c0a9b9e2275d9f8e2fc1af23ce4173a1
footleg <drfootleg@gmail.com>
2023-10-27 22:18:04 +0100

    Initial examples of hardware class implementation

---
+++ RobotHardware_InventorHATmini.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+""" Example of a robot using tank steering with two motors for left and right tracks.
+    This robot uses the Intentor HAT mini board from Pimoroni.
+"""
+from robotinterface import RobotInterface
+from inventorhatmini import InventorHATMini, MOTOR_A, MOTOR_B, NUM_LEDS
+from ioexpander.common import PID, NORMAL_DIR, REVERSED_DIR
+from inventorhatmini.plasma import Plasma
+
+class Robot(RobotInterface):
+    _instance = None
+
+    """ Prevent multiple instances of robot class in same program """
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        # Constants
+        GEAR_RATIO = 50                         # The gear ratio of the motors
+        
+        # Create a new InventorHATMini (this will init the LEDs so needs sudo privileges)
+        self.board = InventorHATMini(motor_gear_ratio=GEAR_RATIO,init_leds=False)
+
+        # Access the motors from Inventor and enable them
+        self.m1 = self.board.motors[MOTOR_A]
+        self.m2 = self.board.motors[MOTOR_B]
+
+        # Set up the encoders
+        self.enc1 = self.board.encoders[MOTOR_A]
+        self.enc2 = self.board.encoders[MOTOR_B]
+
+        # Set the motor and encoder's directions for this robot
+        self.m1.direction(NORMAL_DIR)
+        self.enc1.direction(NORMAL_DIR)
+        self.m2.direction(REVERSED_DIR)
+        self.enc2.direction(REVERSED_DIR)
+
+        # Enable the motors
+        self.m1.enable()
+        self.m2.enable()
+
+    def shutdownHardware(self):
+        # Stop motors
+        self.m1.disable()
+        self.m2.disable()
+        print("Motors disabled")
+        self.setLEDsAllOff()
+
+    def setMotorPower(self, motorIndex: int, power: int):
+        """
+            Set the power for the specified motor of the robot.
+            Motors are referenced by index from 0 to n.
+            Power levels are a percentage of full power, and can be
+            negative to run the motor in reverse. -100 would be full 
+            reverse speed. 100 would be full speed forwards.
+        """
+        if motorIndex == 0:
+            self.m1.speed(-power/100)
+        elif motorIndex == 1:
+            self.m2.speed(power/100)
+
+    def setMotorsPower(self, leftMotor: int, rightMotor: int):
+        """
+            Set the power for the left and right motors of the robot.
+            Power levels are a percentage of full power, and can be
+            negative to run the motor in reverse. -100 would be full 
+            reverse speed. 100 would be full speed forwards.
+        """
+        #set the power for each motor
+        self.m1.speed(-leftMotor/100)
+        self.m2.speed(rightMotor/100)
+    
+    def getEncoderCount(self, motorIndex: int):
+        """
+            Read the position of the specified encoder.
+            Encoders are referenced by index from 0 to n corresponding
+            to the motors.
+            Returns the encoder count for the specified motor.
+        """
+        capture = self.readEncoder(motorIndex)
+            
+        return capture.count
+    
+    def readEncoder(self, motorIndex: int):
+        if motorIndex == 0:
+            capture = self.enc1.capture()
+        elif motorIndex == 1:
+            capture = self.enc2.capture()
+            
+        return capture
+
+    def keepAlive(self):
+        """ 
+            Keep robot alive. This is a watchdog method to tell the robot
+            out code is still talking to it. If this method is not called
+            before the watchdog timeout (on robots with a watchdog safety
+            implementation) then the robot motors will automatically be 
+            powered off.
+        """
+        pass
+
+    def getBatteryVoltage(self) -> float:
+        """ Returns the voltage of the robot battery """
+        return 0
+
+    def getRobotName(self) -> str:
+        """
+            Returns the name of the robot or hardware
+        """
+        return "Triangle Tracks"
+        
+    def getLEDCount(self) -> int:
+        """
+            Returns the LED count of the robot
+        """
+        return NUM_LEDS
+    
+    def setLEDColor(self, ledIdx: int, red: int, green, blue: int):
+        """
+            Set the colour of an RGB LED on the robot
+        """
+        self.board.leds.set_rgb(ledIdx, red, green, blue)
+
+    def showLEDs(self):
+        """
+            Update all LEDs with their programmed colours
+        """
+        self.board.leds.show()
+        
+    def setAllLEDsColor(self, red: int, green: int, blue: int):
+        """
+            Set the colour of all RGB LEDs on the robot
+        """
+        for i in range(NUM_LEDS):
+            self.board.leds.set_rgb(i, red, green, blue)
+        self.showLEDs()
+    
+    def setLEDsAllOff(self):
+        """
+            Set the colour of all RGB LEDs on the robot
+        """
+        self.board.leds.clear()
+        
---
+++ testRobotInterface.py
@@ -0,0 +1,91 @@
+# Simple test program for the robot interface using an Inventor HAT Mini
+from time import sleep
+from RobotHardware_InventorHATmini import Robot
+
+battPowerColour = (0,255,255) # This will get updated to colour indicating battery level
+def showBatteryStatus(v=0):
+    global battPowerColour
+
+    # Set min and max voltage range for colours (min will be shown as red, max as green)
+    BATT_MIN = 6.5
+    BATT_MAX = 8.0
+    battery_voltage = v
+    r = 120
+    g = 60
+    if v == 0 :
+        battery_voltage = robot.getBatteryVoltage()
+        
+    if battery_voltage > 0:
+        batt_percent = 100 * (battery_voltage - BATT_MIN) / (BATT_MAX - BATT_MIN)
+        if batt_percent > 100:
+            batt_percent = 100
+        elif batt_percent < 0:
+            batt_percent = 0
+        r = int(120 - 1.2 * batt_percent)
+        g = int(batt_percent)
+    battPowerColour = (r, g, 0)
+    print(f"Motor supply voltage: {battery_voltage:.2f} Colour: {battPowerColour}")
+
+# Create robot hardware instance
+robot = Robot()
+
+print(f"Robot Name: {robot.getRobotName()}" )
+
+# Get battery voltage and set a colour for LEDs based on this
+showBatteryStatus()
+
+# Report encoders positions
+print(f"Encoders A: {robot.getEncoderCount(0)}; B: {robot.getEncoderCount(1)};")
+
+# Start motor A
+robot.setMotorPower(0,100)
+sleep(2)
+print(f"Encoders A: {robot.getEncoderCount(0)}; B: {robot.getEncoderCount(1)};")
+# Stop motor A and start motor B in reverse
+robot.setMotorsPower(0,-100)
+sleep(2)
+print(f"Encoders A: {robot.getEncoderCount(0)}; B: {robot.getEncoderCount(1)};")
+# Drop motor B to half speed
+robot.setMotorPower(1,-50)
+print(f"Encoders A: {robot.getEncoderCount(0)}; B: {robot.getEncoderCount(1)};")
+sleep(2)
+# Stop both motors
+robot.setMotorsPower(0,0)
+print(f"Encoders A: {robot.getEncoderCount(0)}; B: {robot.getEncoderCount(1)};")
+
+# LED show (3 LED long Larson scanner bar)
+ledPos = 0 #Position of LED scanning cursor used for animation
+ledDir = 1 #Direction LED scanning cursor is moving
+led1 = 2 #Brightness multiple for LED 1
+led2 = 1 #Brightness multiple for LED 2
+led3 = 0.5 #Brightness multiple for LED 3
+ledUpdateInterval = 0
+ledColour = battPowerColour
+
+for i in range(100):
+    #Update LED animation
+    ledUpdateInterval += 1
+    if ledUpdateInterval > 1:
+        ledUpdateInterval = 0
+        robot.setLEDsAllOff()
+        robot.setLEDColor(ledPos,  int(ledColour[0]*led1), int(ledColour[1]*led1), int(ledColour[2]*led1) )
+        ledPos2 = ledPos-ledDir
+        if -1 < ledPos2 < 8:
+            robot.setLEDColor(ledPos2, int(ledColour[0]*led2), int(ledColour[1]*led2), int(ledColour[2]*led2) )
+        ledPos3 = ledPos2-ledDir
+        if -1 < ledPos3 < 8:
+            robot.setLEDColor(ledPos3, int(ledColour[0]*led3), int(ledColour[1]*led3), int(ledColour[2]*led3) )
+        robot.showLEDs()
+        ledPos += ledDir
+        if ledPos > 7:
+            ledDir = -1
+            ledPos = 7
+        elif ledPos < 0:
+            ledDir = 1
+            ledPos = 0
+
+    print(f"LED pos: {ledPos}")
+    sleep(0.01)
+
+
+robot.shutdownHardware()
